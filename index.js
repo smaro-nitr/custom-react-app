1ce804924a38473995a307f6d1e3d25e7ed61893
Subhendu Kumar Sahoo <smaro.nitr@gmail.com>
2019-01-08 18:34:01 +0530

    React form

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ function TableComponent(props) {
     return (
         <div>
             <h3>{props.title}</h3>
-            {props.items.map(item => (
+            {props.items.map((item, index) => (
                 <TableBodyComponent item={item} />
             ))}
         </div>
@@ -207,6 +207,49 @@ class TriviaGame extends React.Component {
     }
 }
 
+/*L1 - Input Components*/
+class ControlledInput extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { inputName: '', checkName: false, textAreaName: '', selectName: '' }
+        this.handleChange = this.handleChange.bind(this)
+    }
+    handleChange(event) {
+        this.setState({ [event.target.name] : [event.target.name][0] === "checkName" ? event.target.checked : event.target.value })
+    }
+    render() {
+        var array = ["apple", "banana", "carrot", "donuts"]
+        var options = array.map((item) =>
+            <option value={item}>{item}</option>
+        )
+
+        return (
+            <div className="card card-body">
+                <div className="form-group">
+                    <input name="inputName" className="form-control" placeholder="enter text here" type="text" value={this.state.inputName} onChange={this.handleChange} disabled={this.state.checkName} />
+                    <label>Your entered text is : {this.state.inputName}</label>
+                </div>
+                <div className="form-check form-check-inline">
+                    <input name="checkName" className="form-check-input" type="checkbox" checked={this.state.checkName} onChange={this.handleChange} />
+                    <label className="form-check-label">finalize input box content : {this.state.checkName.toString()}</label>
+                </div><br />
+                <div className="form-group form-check-inline">
+                    <label>Enter text in text area &nbsp; </label>
+                    <textarea name="testAreaName" type="text" rows="1" value={this.state.testAreaName} onChange={this.handleChange} /> &nbsp; &nbsp;
+                    <label>Your entered text is : {this.state.testAreaName}</label>
+                </div>
+                <div className="form-group">
+                    <select name="selectName" className="form-control" value={this.state.selectName} onChange={this.handleChange}>
+                        <option value="" selected disabled hidden>Choose here</option>
+                        {options}
+                    </select>
+                    <label>Your selected option : {this.state.selectName}</label>
+                </div>
+            </div>
+        )
+    }
+}
+
 /*Final Rendering Element*/
 var renderElement = (
     <div> <br />
@@ -214,6 +257,7 @@ var renderElement = (
         <Welcome name="Subhendu" /> <br />
         <Navigator /> <br />
         <TriviaGame /> <br />
+        <ControlledInput /><br />
     </div>
 )
 
